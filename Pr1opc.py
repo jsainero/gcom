290796b443c8a3ba6a36b57b821bb974f7248fe8
jsainero <jsainero@ucm.es>
2020-02-07 14:04:07 +0100

    Add files via upload

--- Pr1opc.py
+++ Pr1opc.py
@@ -6,18 +6,35 @@ Created on Wed Feb  5 13:41:00 2020
 """
 
 import numpy as np
-import random as rand
-import matplotlib.pyplot as plt
+from colorama import Back
+
+def pintar(a,dim):
+    for i in range(dim):
+        for j in range(dim):
+            if a[i,j]==1:
+                print(Back.BLACK + "  ", end = '')
+            else:
+                print(Back.YELLOW + "  ", end = '')
+        print()
+
+def sierpinski(a,dim):
+    if dim != 1:
+        aux = dim//3
+        a[aux:2*aux,aux:2*aux] = 0
+        for i in range(0,dim,aux):
+            for j in range(0,dim,aux):
+                if i != aux or j != aux:
+                    sierpinski(a[i:i+aux,j:j+aux],aux)
 
 def apartado1():
-    it=2
+    it = 3
     dim = 3**it
     alfombra=np.ones((dim,dim))
-    #for i in range(it):
-    alfombra[3:5][3:5]=0
-    print (alfombra)
+    sierpinski(alfombra,dim)
+    pintar(alfombra,dim)
     return 0;
 
 def apartado2():
     return 0;
+
 apartado1()
