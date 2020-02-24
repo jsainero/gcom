ae4295fa19f39634b716e6afd7695b72e2b98fc2
jorge <jsainero@ucm.es>
2020-02-24 12:40:16 +0100

    Pr3.1 hecha

---
+++ Práctica 3/Pr3.py
@@ -0,0 +1,140 @@
+# -*- coding: utf-8 -*-
+"""
+Plantilla 1 de la práctica 3
+
+Referencia: 
+    https://scikit-learn.org/stable/modules/clustering.html
+    https://scikit-learn.org/stable/modules/generated/sklearn.cluster.KMeans.html
+    https://docs.scipy.org/doc/scipy/reference/spatial.distance.html
+"""
+
+import numpy as np
+
+from sklearn.cluster import DBSCAN
+from sklearn.cluster import KMeans
+from sklearn import metrics
+from sklearn.datasets import make_blobs
+
+import matplotlib.pyplot as plt
+
+# #############################################################################
+# Aquí tenemos definido el sistema X de 1000 elementos de dos estados
+# construido a partir de una muestra aleatoria entorno a unos centros:
+centers = [[1, 1], [-1, -1], [1, -1]]
+X, labels_true = make_blobs(n_samples=1000, centers=centers, cluster_std=0.4,
+                            random_state=0)
+#Si quisieramos estandarizar los valores del sistema, haríamos:
+#from sklearn.preprocessing import StandardScaler
+#X = StandardScaler().fit_transform(X)  
+
+plt.plot(X[:,0],X[:,1],'ro', markersize=1)
+plt.show()
+
+
+def grafica(labels,n_clusters):
+    # Representamos el resultado con un plot
+    unique_labels = set(labels)
+    colors = [plt.cm.Spectral(each)
+              for each in np.linspace(0, 1, len(unique_labels))]
+    
+    plt.figure(figsize=(8,4))
+    for k, col in zip(unique_labels, colors):
+        if k == -1:
+            # Black used for noise.
+            col = [0, 0, 0, 1]
+    
+        class_member_mask = (labels == k)
+    
+        xy = X[class_member_mask]
+        plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
+                 markeredgecolor='k', markersize=5)
+    
+    plt.title('Fixed number of KMeans clusters: %d' % n_clusters)
+    plt.show()
+
+def kMeans(k):
+    global X
+    kmeans = KMeans(n_clusters=k, random_state=0).fit(X)
+    labels = kmeans.labels_
+    if k==1:
+       silhouette = -1
+    else:
+        silhouette = metrics.silhouette_score(X, labels)
+    #print("Los centros de los",k,"clusters son:",kmeans.cluster_centers_)
+    #print("El coeficiente de Silhouette para nuestro sistema de 1000 elementos distribuidos en",k,"clusters es: %0.3f" % silhouette)
+    #grafica(labels,k)
+    return silhouette
+
+def bestSilhouetteKmeans():
+    sils=np.zeros(15)
+    maxi=-1
+    for i in range(1,16):
+        sils[i-1]=kMeans(i)
+        if sils[i-1]>maxi:
+            maxi=sils[i-1]
+            j=i
+    plt.title('Valor de Silhouette en función del número de clusters')
+    plt.plot(np.linspace(1,15,15),sils)
+    plt.show()
+    print("El valor óptimo de Silhouette es %0.3f" % maxi, "y se obtiene con", j, "clusters")
+    
+bestSilhouetteKmeans()
+
+def dbscan(epsilon,metrica):
+    db = DBSCAN(eps=epsilon, min_samples=100, metric=metrica).fit(X)
+    
+    core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+    core_samples_mask[db.core_sample_indices_] = True
+    labels = db.labels_
+    
+    # Number of clusters in labels, ignoring noise if present.
+  
+    
+    #print('Estimated number of clusters: %d' % n_clusters_)
+    #print('Estimated number of noise points: %d' % n_noise_)
+    #print("Adjusted Rand Index: %0.3f" % metrics.adjusted_rand_score(labels_true, labels))
+    if len(set(labels)) == 1:
+        n_clusters_=1
+        silhouette=-1
+    else:
+        n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+        n_noise_ = list(labels).count(-1)
+        silhouette=metrics.silhouette_score(X, labels)
+    #print("Silhouette Coefficient: %0.3f"  % silhouette )
+    return [n_clusters_,silhouette]
+    
+def bestSilhouetteDBSCAN():
+    silsEuc=[[dbscan(i,'euclidean'),i] for i in np.arange(0.1,1.05,0.05)]
+    
+    silsMan=[[dbscan(i,'manhattan'),i] for i in np.arange(0.1,1.05,0.05)]
+    n_clusters_euc=0
+    maxieuc=-1
+    epsilon_euc=0
+    n_clusters_man=0
+    maximan=-1
+    epsilon_man=0
+    
+    for sil in silsEuc:
+        if (sil[0][1]>maxieuc):
+            maxieuc = sil[0][1]
+            n_clusters_euc = sil[0][0]
+            epsilon_euc=sil[1]
+    plt.title('Valor de Silhouette en función del umbral de distancia')
+    plt.plot([silsEuc[i][1] for i in range(len(silsEuc))],[silsEuc[i][0][1] for i in range(len(silsEuc))])
+    plt.show()
+    print("El valor óptimo de Silhouette es %0.3f" % maxieuc, "y se obtiene con umbral de distancia %0.2f" % epsilon_euc, "que da lugar a",n_clusters_euc,"clusters")
+    
+    for sil in silsMan:
+        if (sil[0][1]>maximan):
+            maximan = sil[0][1]
+            n_clusters_man = sil[0][0]
+            epsilon_man=sil[1]
+    plt.title('Valor de Silhouette en función del umbral de distancia')
+    plt.plot([silsMan[i][1] for i in range(len(silsMan))],[silsMan[i][0][1] for i in range(len(silsMan))])
+    plt.show()
+    print("El valor óptimo de Silhouette es %0.3f" % maximan, "y se obtiene con umbral de distancia %0.2f" % epsilon_man, "que da lugar a",n_clusters_man,"clusters")
+
+
+bestSilhouetteDBSCAN()
+
+    
