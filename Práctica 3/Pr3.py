15e0d60823d97d8627a6ba0568a482d3cbd745ec
ldetorre <55990212+ldetorre@users.noreply.github.com>
2020-02-25 10:32:27 +0100

    Update Pr3.py

--- Práctica 3/Pr3.py
+++ Práctica 3/Pr3.py
@@ -30,9 +30,17 @@ X, labels_true = make_blobs(n_samples=1000, centers=centers, cluster_std=0.4,
 plt.plot(X[:,0],X[:,1],'ro', markersize=1)
 plt.show()
 
+'''
+A partir de aquí, código escrito por 
+-Jorge Sainero Valle
+-Lucas de Torre Barrio
+utilizando el código proporcionado
+'''
 
-def grafica(labels,n_clusters):
-    # Representamos el resultado con un plot
+# Representamos el resultado con un plot
+def graficaKMeans(n_clusters):
+    kmeans = KMeans(n_clusters=n_clusters, random_state=0).fit(X)
+    labels = kmeans.labels_
     unique_labels = set(labels)
     colors = [plt.cm.Spectral(each)
               for each in np.linspace(0, 1, len(unique_labels))]
@@ -76,9 +84,34 @@ def bestSilhouetteKmeans():
     plt.title('Valor de Silhouette en función del número de clusters')
     plt.plot(np.linspace(1,15,15),sils)
     plt.show()
-    print("El valor óptimo de Silhouette es %0.3f" % maxi, "y se obtiene con", j, "clusters")
+    print("El valor óptimo de Silhouette con el algoritmo KMeans es %0.3f" % maxi, "y se obtiene con", j, "clusters")
+    graficaKMeans(j);
     
 bestSilhouetteKmeans()
+
+def graficaDBSCAN(epsilon,metrica):
+    db = DBSCAN(eps=epsilon, min_samples=100, metric=metrica).fit(X)    
+    core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+    core_samples_mask[db.core_sample_indices_] = True
+    labels = db.labels_
+    unique_labels = set(labels)
+    colors = [plt.cm.Spectral(each)
+              for each in np.linspace(0, 1, len(unique_labels))]
+    
+    plt.figure(figsize=(8,4))
+    for k, col in zip(unique_labels, colors):
+        if k == -1:
+            # Black used for noise.
+            col = [0, 0, 0, 1]
+    
+        class_member_mask = (labels == k)
+    
+        xy = X[class_member_mask]
+        plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
+                 markeredgecolor='k', markersize=5)
+    
+    #plt.title('Fixed number of KMeans clusters: %d' % n_clusters)
+    plt.show()
 
 def dbscan(epsilon,metrica):
     db = DBSCAN(eps=epsilon, min_samples=100, metric=metrica).fit(X)
@@ -89,16 +122,11 @@ def dbscan(epsilon,metrica):
     
     # Number of clusters in labels, ignoring noise if present.
   
-    
-    #print('Estimated number of clusters: %d' % n_clusters_)
-    #print('Estimated number of noise points: %d' % n_noise_)
-    #print("Adjusted Rand Index: %0.3f" % metrics.adjusted_rand_score(labels_true, labels))
     if len(set(labels)) == 1:
         n_clusters_=1
         silhouette=-1
     else:
         n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
-        n_noise_ = list(labels).count(-1)
         silhouette=metrics.silhouette_score(X, labels)
     #print("Silhouette Coefficient: %0.3f"  % silhouette )
     return [n_clusters_,silhouette]
@@ -123,6 +151,7 @@ def bestSilhouetteDBSCAN():
     plt.plot([silsEuc[i][1] for i in range(len(silsEuc))],[silsEuc[i][0][1] for i in range(len(silsEuc))])
     plt.show()
     print("El valor óptimo de Silhouette es %0.3f" % maxieuc, "y se obtiene con umbral de distancia %0.2f" % epsilon_euc, "que da lugar a",n_clusters_euc,"clusters")
+    graficaDBSCAN(epsilon_euc,'euclidean')
     
     for sil in silsMan:
         if (sil[0][1]>maximan):
